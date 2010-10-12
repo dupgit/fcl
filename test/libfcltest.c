b7a2c0dfac6758b7ae08ef0cf9e22b0db287a115
dup <dup@localhost>
2010-10-12 20:17:05 +0000

    Translatable project Testing reading part of a file (reading more than allowed by the library)

--- test/libfcltest.c
+++ test/libfcltest.c
@@ -24,19 +24,46 @@
 /** @file libfcltest.c
  */
 
+#include "config.h"
+
 #include <stdio.h>
+#include <glib/gi18n-lib.h>
+
 #include <fcl.h>
 
 #include "libfcltest.h"
 
+static void print_message(gboolean success, const char *format, ...);
+static void init_international_languages(void);
+
 static void test_openning_and_closing_files(void);
 static void test_openning_and_reading_files(void);
-static void print_message(gboolean success, const char *format, ...);
+
+
+/**
+ *  Inits internationalisation
+ */
+static void init_international_languages(void)
+{
+    gchar *result = NULL;
+
+    setlocale(LC_ALL, "");
+    result = bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
+
+    fprintf(stdout, Q_("Gettext package : %s\n"), GETTEXT_PACKAGE);
+    fprintf(stdout, Q_("Locale dir : %s\n"), LOCALEDIR);
+    fprintf(stdout, Q_("Bindtextdomain : %s\n"), result);
+
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain(GETTEXT_PACKAGE);
+}
+
 
 /**
  * Prints a message from a test
  * @param success indicates whether the test was sucessfull or not
- * @param message the message to print
+ * @param format the message to print in a vprintf format
+ * @param ... the va_list : list of arguments to fill in the format
  */
 static void print_message(gboolean success, const char *format, ...)
 {
@@ -49,12 +76,12 @@ static void print_message(gboolean success, const char *format, ...)
 
     if (success)
         {
-            fprintf(stdout, "[ OK ] ");
+            fprintf(stdout, Q_("[ OK ] "));
             fprintf(stdout, "%s\n", str);
         }
     else
         {
-            fprintf(stdout, "[FAIL] ");
+            fprintf(stdout, Q_("[FAIL] "));
             fprintf(stdout, "%s\n", str);
         }
 }
@@ -69,20 +96,20 @@ static void test_openning_and_closing_files(void)
     my_test_file = fcl_open_file("/bin/bash", LIBFCL_MODE_READ);
     if (my_test_file != NULL)
         {
-            print_message(my_test_file != NULL, "Openning a file in read mode (%Ld)", my_test_file->real_size);
+            print_message(my_test_file != NULL, Q_("Openning a file in read mode (%Ld)"), my_test_file->real_size);
         }
     else
         {
-            print_message(my_test_file != NULL, "Openning a file in read mode.");
+            print_message(my_test_file != NULL, Q_("Openning a file in read mode."));
         }
     fcl_close_file(my_test_file);
 
     my_test_file = fcl_open_file("/tmp/test.libfcl", LIBFCL_MODE_WRITE);
-    print_message(my_test_file != NULL, "Openning a file in write mode.");
+    print_message(my_test_file != NULL, Q_("Openning a file in write mode."));
     fcl_close_file(my_test_file);
 
     my_test_file = fcl_open_file("/tmp/test.libfcl", LIBFCL_MODE_CREATE);
-    print_message(my_test_file != NULL, "Openning a file in create mode.");
+    print_message(my_test_file != NULL, Q_("Openning a file in create mode."));
 
     fcl_close_file(my_test_file);
 }
@@ -96,26 +123,39 @@ static void test_openning_and_reading_files(void)
     fcl_file_t *my_test_file = NULL;
     fcl_buf_t *buffer = NULL;
 
+
+    /* A valid test. Should return ELF as this is the magic number for a compiled /bin/bash */
     my_test_file = fcl_open_file("/bin/bash", LIBFCL_MODE_READ);
     buffer = fcl_read_bytes(my_test_file, 1, 3);
 
     if (buffer != NULL && buffer->buffer != NULL)
         {
-            print_message(buffer != NULL, "Read : %s", buffer->buffer);
+            print_message(buffer != NULL, Q_("Read (%d bytes at %d) : '%s'"), buffer->buf_size, buffer->offset, buffer->buffer);
         }
     else
         {
-            print_message(buffer != NULL, "Reading 3 bytes in /bin/bash !");
+            print_message(buffer != NULL, Q_("Reading 3 bytes in /bin/bash !"));
         }
+    fcl_close_file(my_test_file);
+
+    /* An Invalid test. Sould return NULL */
+    my_test_file = fcl_open_file("/bin/bash", LIBFCL_MODE_READ);
+    buffer = fcl_read_bytes(my_test_file, 1, LIBFCL_MAX_BUF_SIZE + 1);
+    print_message(buffer == NULL, Q_("Openning a file in create mode and reading more than allowed."));
+    fcl_close_file(my_test_file);
 }
 
 
 int main(int argc, char **argv)
 {
+    /* Initializing the locales */
+    init_international_languages();
 
-    fprintf(stdout, "Testing libfcl ...\n");
+    /* Initializing the library */
+    fprintf(stdout, Q_("\nNow testing libfcl ...\n"));
     libfcl_initialize();
 
+    /**** Tests ****/
     test_openning_and_closing_files();
 
     test_openning_and_reading_files();
